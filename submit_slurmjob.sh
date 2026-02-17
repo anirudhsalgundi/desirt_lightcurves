1c57f3c7cbdf82673c5c363416edbf4ddb0b1bbc
anirudhsalgundi <anirudhsalgundi@gmail.com>
2026-02-16 23:14:58 -0500

    basic script for submitting slurm job, tested with a test run

--- submit_slurmjob.sh
+++ submit_slurmjob.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-mkdir -p logs
-
-
+#SBATCH --ntasks-per-node=12
 cd /ocean/projects/phy250012p/salgundi/desirt_lightcurves
 source .venv/bin/activate
 
-
 time uv run organize_desirt_data.py \
-    --csv_path ./data/test_csvs.txt \
-    --fits_path ./data/test_fits.txt \
-    --n_workers 6
+    --csv_path /ocean/projects/phy250012p/salgundi/desirt_lightcurves/data/candidate_summary_csvs.txt \
+    --fits_path /ocean/projects/phy250012p/salgundi/desirt_lightcurves/data/candidate_fits_files.txt \
+    --n_workers 12
