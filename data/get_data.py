904174289842906f2f45c7cfb93432a451ef6596
anirudhsalgundi <anirudhsalgundi@gmail.com>
2026-02-17 14:48:22 -0500

    Remove test file writing for candidate summary CSVs and FITS files from get_data.py

--- data/get_data.py
+++ data/get_data.py
@@ -17,15 +17,6 @@ with open("candidate_fits_files.txt", "w") as f:
     for fits_file in candidate_fits_files:
         f.write(str(fits_file) + "\n")
 
-# Write just the first file (or a subset) to test files
-with open("test_candidate_summary_csvs.txt", "w") as f:
-    if candidate_summary_csvs:  # Check if list is not empty
-        f.write(str(candidate_summary_csvs[0]) + "\n")
-
-with open("test_candidate_fits_files.txt", "w") as f:
-    if candidate_fits_files:  # Check if list is not empty
-        f.write(str(candidate_fits_files[0]) + "\n")
-
 
 with open("latest_summary.txt", "w") as f:
     f.write(f"Found {len(candidate_summary_csvs)} candidate summary CSV files.\n")
